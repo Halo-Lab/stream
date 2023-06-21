9921dc57d2a51f6d84723a9987f12ae649747a12
Yevhen Kapelianovych <kapelianovych.y.v@protonmail.com>
2023-06-21 23:22:49 +0300

    Fix the distinct function. It sent only first stream value and discarded others. Format code.

--- index.d.ts
+++ index.d.ts
@@ -1,160 +1,160 @@
 export interface Sink<A> {
-  (value: A): void
+  (value: A): void;
 }
 
 export interface Stream<A> {
-  (sink: Sink<A>): VoidFunction
-  readonly '@@stream': Stream<A>
+  (sink: Sink<A>): VoidFunction;
+  readonly "@@stream": Stream<A>;
 }
 
 export function from<A>(
   source: (push: Sink<A>) => void | VoidFunction
-): Stream<A>
+): Stream<A>;
 
-export function isStream<A>(value: Stream<A>): true
-export function isStream<A>(value: unknown): value is Stream<A>
+export function isStream<A>(value: Stream<A>): true;
+export function isStream<A>(value: unknown): value is Stream<A>;
 
 export function of<const T extends readonly unknown[]>(
   ...values: T
-): Stream<T[number]>
+): Stream<T[number]>;
 
 export function map<A, B>(
   callback: (value: A) => B
-): (source: Stream<A>) => Stream<B>
+): (source: Stream<A>) => Stream<B>;
 export function map<A, B>(
   source: Stream<A>,
   callback: (value: A) => B
-): Stream<B>
+): Stream<B>;
 
 export function filter(
   callback: BooleanConstructor
 ): <A>(
   source: Stream<A>
-) => Stream<Exclude<A, null | undefined | 0 | '' | false>>
+) => Stream<Exclude<A, null | undefined | 0 | "" | false>>;
 export function filter<A, B extends A>(
   callback: (value: A) => value is B
-): (source: Stream<A>) => Stream<B>
+): (source: Stream<A>) => Stream<B>;
 export function filter<A>(
   callback: (value: A) => boolean
-): (source: Stream<A>) => Stream<A>
+): (source: Stream<A>) => Stream<A>;
 export function filter<A>(
   source: Stream<A>,
   callback: BooleanConstructor
-): Stream<Exclude<A, null | undefined | 0 | '' | false>>
+): Stream<Exclude<A, null | undefined | 0 | "" | false>>;
 export function filter<A, B extends A>(
   source: Stream<A>,
   callback: (value: A) => value is B
-): Stream<B>
+): Stream<B>;
 export function filter<A>(
   source: Stream<A>,
   callback: (value: A) => boolean
-): Stream<A>
+): Stream<A>;
 
 export function forEach<A>(
   callback: Sink<A>
-): (source: Stream<A>) => VoidFunction
-export function forEach<A>(source: Stream<A>, callback: Sink<A>): VoidFunction
+): (source: Stream<A>) => VoidFunction;
+export function forEach<A>(source: Stream<A>, callback: Sink<A>): VoidFunction;
 
 export function scan<A>(
   reduce: (accumulator: A, value: A) => A
-): (source: Stream<A>) => Stream<A>
+): (source: Stream<A>) => Stream<A>;
 export function scan<A, B>(
   accumulator: B,
   reduce: (accumulator: B, value: A) => B
-): (source: Stream<A>) => Stream<B>
+): (source: Stream<A>) => Stream<B>;
 export function scan<A>(
   source: Stream<A>,
   reduce: (accumulator: A, value: A) => A
-): Stream<A>
+): Stream<A>;
 export function scan<A, B>(
   source: Stream<A>,
   accumulator: B,
   reduce: (accumulator: B, value: A) => B
-): Stream<B>
+): Stream<B>;
 
-export function take(amount: number): <A>(source: Stream<A>) => Stream<A>
-export function take<A>(source: Stream<A>, amount: number): Stream<A>
+export function take(amount: number): <A>(source: Stream<A>) => Stream<A>;
+export function take<A>(source: Stream<A>, amount: number): Stream<A>;
 
-export function skip(amount: number): <A>(source: Stream<A>) => Stream<A>
-export function skip<A>(source: Stream<A>, amount: number): Stream<A>
+export function skip(amount: number): <A>(source: Stream<A>) => Stream<A>;
+export function skip<A>(source: Stream<A>, amount: number): Stream<A>;
 
 export function takeWhile<A>(
   predicate: (value: A) => boolean
-): (source: Stream<A>) => Stream<A>
+): (source: Stream<A>) => Stream<A>;
 export function takeWhile<A>(
   source: Stream<A>,
   predicate: (value: A) => boolean
-): Stream<A>
+): Stream<A>;
 
 export function skipWhile<A>(
   predicate: (value: A) => boolean
-): (source: Stream<A>) => Stream<A>
+): (source: Stream<A>) => Stream<A>;
 export function skipWhile<A>(
   source: Stream<A>,
   predicate: (value: A) => boolean
-): Stream<A>
+): Stream<A>;
 
 export function unique<A, K = A>(
   selector?: (value: A) => K,
   flushes?: Stream<unknown>
-): (source: Stream<A>) => Stream<A>
+): (source: Stream<A>) => Stream<A>;
 export function unique<A, K = A>(
   source: Stream<A>,
   selector?: (value: A) => K,
   flushes?: Stream<unknown>
-): Stream<A>
+): Stream<A>;
 
 export function merge<B>(
   other: Stream<B>
-): <A = B>(source: Stream<A>) => Stream<A | B>
+): <A = B>(source: Stream<A>) => Stream<A | B>;
 export function merge<A, B = A>(
   source: Stream<A>,
   other: Stream<B>
-): Stream<A | B>
+): Stream<A | B>;
 
 export function distinct<A>(
   compare?: (previous: A, next: A) => boolean
-): (source: Stream<A>) => Stream<A>
+): (source: Stream<A>) => Stream<A>;
 export function distinct<A>(
   source: Stream<A>,
   compare?: (previous: A, next: A) => boolean
-): Stream<A>
+): Stream<A>;
 
-type _Sink<A> = Sink<A>
+type _Sink<A> = Sink<A>;
 
-type _of = typeof of
-type _is = typeof isStream
-type _map = typeof map
-type _from = typeof from
-type _scan = typeof scan
-type _take = typeof take
-type _skip = typeof skip
-type _merge = typeof merge
-type _filter = typeof filter
-type _unique = typeof unique
-type _forEach = typeof forEach
-type _distinct = typeof distinct
-type _skipWhile = typeof skipWhile
-type _takeWhile = typeof takeWhile
+type _of = typeof of;
+type _is = typeof isStream;
+type _map = typeof map;
+type _from = typeof from;
+type _scan = typeof scan;
+type _take = typeof take;
+type _skip = typeof skip;
+type _merge = typeof merge;
+type _filter = typeof filter;
+type _unique = typeof unique;
+type _forEach = typeof forEach;
+type _distinct = typeof distinct;
+type _skipWhile = typeof skipWhile;
+type _takeWhile = typeof takeWhile;
 
 declare namespace Stream {
-  export type Self<A> = Stream<A>
-  export type Sink<A> = _Sink<A>
+  export type Self<A> = Stream<A>;
+  export type Sink<A> = _Sink<A>;
 
-  export const of: _of
-  export const is: _is
-  export const map: _map
-  export const from: _from
-  export const scan: _scan
-  export const take: _take
-  export const skip: _skip
-  export const merge: _merge
-  export const filter: _filter
-  export const unique: _unique
-  export const forEach: _forEach
-  export const distinct: _distinct
-  export const skipWhile: _skipWhile
-  export const takeWhile: _takeWhile
+  export const of: _of;
+  export const is: _is;
+  export const map: _map;
+  export const from: _from;
+  export const scan: _scan;
+  export const take: _take;
+  export const skip: _skip;
+  export const merge: _merge;
+  export const filter: _filter;
+  export const unique: _unique;
+  export const forEach: _forEach;
+  export const distinct: _distinct;
+  export const skipWhile: _skipWhile;
+  export const takeWhile: _takeWhile;
 }
 
-export default Stream
+export default Stream;
--- index.js
+++ index.js
@@ -1,58 +1,58 @@
-const STREAM = '@@stream'
+const STREAM = "@@stream";
 
 export function from(source) {
   const stream = (sink) => {
-    const stop = source(sink)
+    const stop = source(sink);
 
-    return () => stop?.()
-  }
+    return () => stop?.();
+  };
 
-  stream[STREAM] = stream
+  stream[STREAM] = stream;
 
-  return stream
+  return stream;
 }
 
 export function isStream(value) {
-  return typeof value === 'function' && value === value[STREAM]
+  return typeof value === "function" && value === value[STREAM];
 }
 
 export function of(...values) {
   return from((push) => {
-    const iterator = values[Symbol.iterator]()
+    const iterator = values[Symbol.iterator]();
 
     Promise.resolve().then(() => {
-      for (const value of iterator) push(value)
-    })
+      for (const value of iterator) push(value);
+    });
 
     return () => {
-      iterator.return()
-    }
-  })
+      iterator.return();
+    };
+  });
 }
 
 export function map(source, callback) {
   return callback
     ? from((push) => source((value) => push(callback(value))))
-    : (anotherSource) => map(anotherSource, source)
+    : (anotherSource) => map(anotherSource, source);
 }
 
 export function filter(source, callback) {
   return callback
     ? from((push) => source((value) => callback(value) && push(value)))
-    : (anotherSource) => filter(anotherSource, source)
+    : (anotherSource) => filter(anotherSource, source);
 }
 
 export function forEach(source, callback) {
   return callback
     ? source(callback)
-    : (anotherSource) => forEach(anotherSource, source)
+    : (anotherSource) => forEach(anotherSource, source);
 }
 
 export function scan(source, accumulator, reduce = accumulator) {
   return isStream(source)
     ? from((push) => {
-        let created = true
-        let intermediate = accumulator
+        let created = true;
+        let intermediate = accumulator;
 
         return source((value) => {
           push(
@@ -60,120 +60,120 @@ export function scan(source, accumulator, reduce = accumulator) {
               created && intermediate === reduce
                 ? value
                 : reduce(intermediate, value))
-          )
+          );
 
-          if (created) created = false
-        })
+          if (created) created = false;
+        });
       })
-    : (anotherSource) => scan(anotherSource, source, accumulator)
+    : (anotherSource) => scan(anotherSource, source, accumulator);
 }
 
 export function take(source, amount) {
-  let _amount = amount
+  let _amount = amount;
 
   return isStream(source)
     ? takeWhile(source, () => _amount-- > 0 || ((_amount = amount), false))
-    : (anotherSource) => take(anotherSource, source)
+    : (anotherSource) => take(anotherSource, source);
 }
 
 export function takeWhile(source, callback) {
   return callback
     ? from((push) => {
-        let stop
-        let cleared
+        let stop;
+        let cleared;
 
         const clear = () => {
-          stop?.()
-          stop = null
-          cleared = true
-        }
+          stop?.();
+          stop = null;
+          cleared = true;
+        };
 
         stop = source(
           (value) => cleared || (callback(value) ? push(value) : clear())
-        )
+        );
 
-        return clear
+        return clear;
       })
-    : (anotherSource) => takeWhile(anotherSource, source)
+    : (anotherSource) => takeWhile(anotherSource, source);
 }
 
 export function skip(source, amount) {
   return isStream(source)
     ? from((push) => {
-        let _amount = amount
+        let _amount = amount;
 
-        return source((value) => (_amount > 0 ? _amount-- : push(value)))
+        return source((value) => (_amount > 0 ? _amount-- : push(value)));
       })
-    : (anotherSource) => skip(anotherSource, source)
+    : (anotherSource) => skip(anotherSource, source);
 }
 
 export function skipWhile(source, callback) {
   return callback
     ? from((push) => {
-        let skipping = true
+        let skipping = true;
 
         return source((value) =>
           skipping ? (skipping = callback(value)) || push(value) : push(value)
-        )
+        );
       })
-    : (anotherSource) => skipWhile(anotherSource, source)
+    : (anotherSource) => skipWhile(anotherSource, source);
 }
 
 export function unique(source, selector, flushes) {
   return isStream(source)
     ? from((push) => {
-        const keys = new Set()
+        const keys = new Set();
 
         const stopFiltering = source((value) => {
-          const key = selector?.(value) ?? value
+          const key = selector?.(value) ?? value;
 
           if (!keys.has(key)) {
-            keys.add(key)
+            keys.add(key);
 
-            push(value)
+            push(value);
           }
-        })
+        });
 
-        const stopFlushing = flushes?.(() => keys.clear())
+        const stopFlushing = flushes?.(() => keys.clear());
 
         return () => {
-          keys.clear()
-          stopFiltering()
-          stopFlushing?.()
-        }
+          keys.clear();
+          stopFiltering();
+          stopFlushing?.();
+        };
       })
-    : (anotherSource) => unique(anotherSource, source, selector)
+    : (anotherSource) => unique(anotherSource, source, selector);
 }
 
 export function merge(source, other) {
   return other
     ? from((push) => {
-        const stopSource = source(push)
-        const stopOther = other(push)
+        const stopSource = source(push);
+        const stopOther = other(push);
 
         return () => {
-          stopSource()
-          stopOther()
-        }
+          stopSource();
+          stopOther();
+        };
       })
-    : (actualSource) => merge(actualSource, source)
+    : (actualSource) => merge(actualSource, source);
 }
 
 export function distinct(source, compare = Object.is) {
   return isStream(source)
     ? from((push) => {
-        let firstSent = false
-        let previous
+        let firstSent = false;
+        let previous;
 
         return source((value) => {
-          previous = value
-
           firstSent
             ? compare(previous, value) || push(value)
-            : ((firstSent = true), push(value))
-        })
+            : ((firstSent = true), push(value));
+
+          previous = value;
+        });
       })
-    : (anotherSource) => distinct(anotherSource, source ?? compare)
+    : (anotherSource) => distinct(anotherSource, source ?? compare);
 }
 
 export default {
@@ -191,4 +191,4 @@ export default {
   distinct,
   skipWhile,
   takeWhile,
-}
+};
